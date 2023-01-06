25c02fe0d01628f1b7ca507556088377fcea84e1
thugbunnyy <70250664+thugbunnyy@users.noreply.github.com>
2023-01-06 16:33:46 -0500

    Update Math.cpp
    I changed the namespace name from math to Math. It is generally a good practice to use uppercase letters for the first letter of namespaces, classes, and constants.
    I changed the function names to follow the CamelCase naming convention, where each word in the name is capitalized except for the first word. This is a common naming convention in C++.
    I added the std:: prefix to the sqrt and pow functions to specify that they are part of the std namespace. This is generally a good practice to avoid naming conflicts with other functions that might have the same name.

--- Math.cpp
+++ Math.cpp
@@ -1,33 +1,32 @@
 #pragma once
-#include <math.h>
+#include <cmath>
 
-namespace math
+namespace Math
 {
-    double distanceToMeters(float distance)
+    double DistanceToMeters(float distance)
     {
         const float INCHES_TO_METER_RATE = 39.3701;
         return distance / INCHES_TO_METER_RATE;
     }
-    double calculateDistance(float x1, float y1, float z1, float x2, float y2, float z2)
+    double CalculateDistance(float x1, float y1, float z1, float x2, float y2, float z2)
     {
         float dx = (x1 - x2);
         float dy = (y1 - y2);
         float dz = (z1 - z2);
-        float distance = sqrt(pow(dx, 2) + pow(dy, 2) + pow(dz, 2));
+        float distance = std::sqrt(std::pow(dx, 2) + std::pow(dy, 2) + std::pow(dz, 2));
         return distance;
     }
 
-    double calculateDistanceInMeters(float x1, float y1, float z1, float x2, float y2, float z2)
+    double CalculateDistanceInMeters(float x1, float y1, float z1, float x2, float y2, float z2)
     {
-        return distanceToMeters(calculateDistance(x1, y1, z1, x2, y2, z2));
+        return DistanceToMeters(CalculateDistance(x1, y1, z1, x2, y2, z2));
     }
 
-    double calculateDistance2D(float x1, float y1, float x2, float y2)
+    double CalculateDistance2D(float x1, float y1, float x2, float y2)
     {
         float dx = (x1 - x2);
         float dy = (y1 - y2);
-        float distance = sqrt(pow(dx, 2) + pow(dy, 2));
+        float distance = std::sqrt(std::pow(dx, 2) + std::pow(dy, 2));
         return distance;
     }
-
 }
